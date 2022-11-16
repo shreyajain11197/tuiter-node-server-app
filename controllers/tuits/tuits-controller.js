84c23f64045fecb963831fd5d086021b61a3cc55
Shreya Bharath Jain <62557509+shreyajain11197@users.noreply.github.com>
2022-11-15 19:53:13 -0500

    Committing Tuiter part of Assignment 8

---
+++ controllers/tuits/tuits-controller.js
@@ -0,0 +1,39 @@
+import posts from "./tuits.js";
+let tuits = posts;
+
+export default (app) => {
+    app.post('/api/tuits', createTuit);
+    app.get('/api/tuits', findTuits);
+    app.put('/api/tuits/:tid', updateTuit);
+    app.delete('/api/tuits/:tid', deleteTuit);
+}
+
+const findTuits = (req,res) => {
+    res.json(tuits);
+}
+
+const createTuit = (req, res) => {
+    const newTuit = req.body;
+    newTuit._id = (new Date()).getTime()+'';
+    newTuit.likes = 0;
+    newTuit.liked = false;
+    tuits.push(newTuit);
+    res.json(tuits);
+}
+
+const updateTuit = (req, res) => {
+    const tuitdIdToUpdate = req.params.tid;
+    const updates = req.body;
+    const tuitIndex = tuits.findIndex(
+        (t) => t._id.toString() === tuitdIdToUpdate)
+    tuits[tuitIndex] =
+        {...tuits[tuitIndex], ...updates};
+    res.sendStatus(200);
+}
+
+const deleteTuit = (req, res) => {
+    const tuitId = req.params['tid'];
+    tuits = tuits.filter(ts =>
+        ts._id.toString() !== tuitId)
+    res.sendStatus(200);
+}
