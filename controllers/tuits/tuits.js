b6070e327733bffc1ac8a21c5342b4092adde657
Shreya Bharath Jain <62557509+shreyajain11197@users.noreply.github.com>
2022-11-18 09:24:42 -0500

    Committing Tuiter part of Assignment 8

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -2,17 +2,25 @@ export default [
     {
         "_id": 123, "topic": "Space", "username": "SpaceX",
         "handle": "@spacex", "time": "2h", "image": "spacex.png",
-        "title": "SpaceX's Mission",
+        "title": "Tesla cybertruck lands on Mars and picks up the Curiosity rover on its 6' bed.",
         "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
-        "liked": true, "likes": 2345,
-        "replies": 123, "retuits": 432
+        "liked": true, "likes": 2345, "dislikes": 50, "disliked" : false,
+        "replies": 123, "retuits": 567
     },
     {
         "_id": 234, "topic": "Traffic", "username": "The Boring Company",
-        "handle": "@boringcompany", "time": "2h", "image": "boring2.jpg",
+        "handle": "@boringcompany", "time": "2h", "image": "boring2.jpeg",
         "title": "100s of SpaceX Starships land on Mars after a 6 month journey. 1000s of Martian colonists being building Mars Base 1",
         "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
-        "liked": true, "likes": 2345,
-        "replies": 123, "retuits": 432
+        "liked": false, "likes": 470, "dislikes": 230, "disliked" : true,
+        "replies": 234, "retuits": 432
     },
+    {
+        "_id": 456, "topic": "Tesla", "username": "Tesla",
+        "handle": "@tesla", "time": "2h", "image": "tesla-icon.jpeg",
+        "title": "Autopilot is an advanced driver assistance system that assists your car with steering.",
+        "tuit": "Autopilot is an advanced driver assistance system that assists your car with steering, accelerating and braking for other vehicles and pedestrians within its lane. It assists with the most burdensome parts of driving and work alongside features like emergency braking, collision warning.",
+        "liked": true, "likes": 400, "dislikes": 0, "disliked" : false,
+        "replies": 337, "retuits": 12
+    }
 ];
