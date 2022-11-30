38b3913f9e109158753ccf42980be3b3d334232c
Shreya Bharath Jain <62557509+shreyajain11197@users.noreply.github.com>
2022-11-30 18:59:48 -0500

    Committing Tuiter part of Assignment 9

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -3,6 +3,15 @@ const schema = mongoose.Schema({
     tuit: String,
     likes: Number,
     liked: Boolean,
+    image: String,
+    topic: String,
+    userName: String,
+    time: String,
+    title: String,
+    replies: String,
+    retuits: String,
+    handle: String,
+    dislikes: Number,
 }, {collection: 'tuits'});
 
 export default schema;
